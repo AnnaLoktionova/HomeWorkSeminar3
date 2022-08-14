613f0fb0bf5a1dc49daa0232f4b1a16181f7ceca
Anna <galianova-anna@yandex.ru>
2022-08-14 20:42:19 +0300

    Task 2 is ready

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,31 +1,20 @@
 ﻿/* Напишите программу, которая принимает на вход координаты двух точек и 
-находит расстояние между ними в 3D пространстве.
+находит расстояние между ними в 3D пространстве*/
 
-Console.WriteLine("Введите координату X1:");
-double X1 = double.Parse(Console.ReadLine());
-Console.WriteLine("Введите координату Y1:");
-double Y1 = double.Parse(Console.ReadLine());
-Console.WriteLine("Введите координату Z1:");
-double Z1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату x1");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату y1");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату z1");
+int z1 = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Введите координату X2:");
-double X2 = double.Parse(Console.ReadLine());
-Console.WriteLine("Введите координату Y2:");
-double Y2 = double.Parse(Console.ReadLine());
-Console.WriteLine("Введите координату Z2:");
-double Z2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату x2");
+int x2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату y2");
+int y2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату z2");
+int z2 = int.Parse(Console.ReadLine());
 
-double result = Math.Sqrt(Math.Pow((X2 - X1),2) + Math.Pow((Y2 - Y2),2) + Math.Pow((Z2 - Z1),2));
+double result = Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2) + Math.Pow((z2 - z1),2));
 
-Console.WriteLine($"result = {result:f2}");*/
-
-
-Console.WriteLine("Введите координаты точка A");
-double A = double.Parse(Console.ReadLine());
-string AA = Convert.ToString(A);
-Console.WriteLine("Введите координаты точка B");
-double B = double.Parse(Console.ReadLine());
-string BB = Convert.ToString(B);
-
-double result = Math.Sqrt(Math.Pow(BB[1] - AA[1],2) + Math.Pow(BB[2] - AA[2],2) + Math.Pow(BB[3] - AA[3],2));
-Console.WriteLine(result);
+Console.WriteLine($"result = {result:f2}");
