8e87520c231e5f20f01b197c76311282fccfbfb9
Anna <galianova-anna@yandex.ru>
2022-08-14 15:45:47 +0300

    Task 1 is ready

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Написать программу, которая принимает на вход пятизначное число и 
+проверяет, является ли оно палиндромом*/
+Console.WriteLine("Введите пятизначное число: ");
+int n = int.Parse(Console.ReadLine());
+string n1 = Convert.ToString(n);
+if (n1[0] == n1[4] && n1[1] == n1[3]){
+    Console.WriteLine("да");
+}
+else{
+    Console.WriteLine("нет");
+}
