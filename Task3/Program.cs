b0873c14c08a727410b609f5a26187c3b0010749
Anna <galianova-anna@yandex.ru>
2022-08-14 19:58:34 +0300

    Task 3 is ready

---
+++ Task3/Program.cs
@@ -0,0 +1,7 @@
+﻿// написать программу, которая принимает на вход число N и выдает таблицу кубов от 1 до N
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+for(int a = 1; a <= N; a++){
+    Console.WriteLine(a * a * a);
+}
